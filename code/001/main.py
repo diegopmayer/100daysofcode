8417fa35351355b735dfa9a2a16e1bf8a2ff7fab
diegopmayer <diegopmayer@gmail.com>
2024-05-07 22:48:43 -0300

    day 01

---
+++ code/001/main.py
@@ -0,0 +1,5 @@
+# Writing a solution where ask for where are you up and your pet's name and concatened them
+print("Welcome to the Band Name Generator")
+local_grew_up = input("What's the name of city you grew up in?\n")
+pet_name = input("What's your pet's name?\n")
+print("Your band name could be " + local_grew_up + " " + pet_name)
